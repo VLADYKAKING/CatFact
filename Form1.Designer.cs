f993f5b51cd9a57936952d8f08a6ba0910b3ce53
VLADYKAKING <85833454+VLADYKAKING@users.noreply.github.com>
2021-06-13 20:31:07 +0600

    creation

---
+++ Form1.Designer.cs
@@ -0,0 +1,212 @@
+﻿
+namespace CatFact
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.back = new System.Windows.Forms.Button();
+            this.forward = new System.Windows.Forms.Button();
+            this.tabControl1 = new System.Windows.Forms.TabControl();
+            this.tabPage1 = new System.Windows.Forms.TabPage();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.tabPage2 = new System.Windows.Forms.TabPage();
+            this.button2 = new System.Windows.Forms.Button();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.tabControl1.SuspendLayout();
+            this.tabPage1.SuspendLayout();
+            this.tabPage2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(141, 227);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(207, 33);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Запросить факты о котах";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // back
+            // 
+            this.back.Location = new System.Drawing.Point(6, 227);
+            this.back.Name = "back";
+            this.back.Size = new System.Drawing.Size(129, 33);
+            this.back.TabIndex = 2;
+            this.back.Text = "<<< Назад";
+            this.back.UseVisualStyleBackColor = true;
+            this.back.Click += new System.EventHandler(this.back_Click);
+            // 
+            // forward
+            // 
+            this.forward.Location = new System.Drawing.Point(354, 227);
+            this.forward.Name = "forward";
+            this.forward.Size = new System.Drawing.Size(129, 33);
+            this.forward.TabIndex = 3;
+            this.forward.Text = "Вперед >>>";
+            this.forward.UseVisualStyleBackColor = true;
+            this.forward.Click += new System.EventHandler(this.forward_Click);
+            // 
+            // tabControl1
+            // 
+            this.tabControl1.Controls.Add(this.tabPage1);
+            this.tabControl1.Controls.Add(this.tabPage2);
+            this.tabControl1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tabControl1.Location = new System.Drawing.Point(0, 0);
+            this.tabControl1.Name = "tabControl1";
+            this.tabControl1.SelectedIndex = 0;
+            this.tabControl1.Size = new System.Drawing.Size(497, 292);
+            this.tabControl1.TabIndex = 4;
+            // 
+            // tabPage1
+            // 
+            this.tabPage1.Controls.Add(this.richTextBox1);
+            this.tabPage1.Controls.Add(this.back);
+            this.tabPage1.Controls.Add(this.forward);
+            this.tabPage1.Controls.Add(this.button1);
+            this.tabPage1.Location = new System.Drawing.Point(4, 22);
+            this.tabPage1.Name = "tabPage1";
+            this.tabPage1.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage1.Size = new System.Drawing.Size(489, 266);
+            this.tabPage1.TabIndex = 0;
+            this.tabPage1.Text = "New facts";
+            this.tabPage1.UseVisualStyleBackColor = true;
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.richTextBox1.Location = new System.Drawing.Point(3, 3);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(483, 214);
+            this.richTextBox1.TabIndex = 4;
+            this.richTextBox1.Text = "";
+            // 
+            // tabPage2
+            // 
+            this.tabPage2.Controls.Add(this.button5);
+            this.tabPage2.Controls.Add(this.button4);
+            this.tabPage2.Controls.Add(this.button3);
+            this.tabPage2.Controls.Add(this.dataGridView1);
+            this.tabPage2.Controls.Add(this.button2);
+            this.tabPage2.Location = new System.Drawing.Point(4, 22);
+            this.tabPage2.Name = "tabPage2";
+            this.tabPage2.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage2.Size = new System.Drawing.Size(489, 266);
+            this.tabPage2.TabIndex = 1;
+            this.tabPage2.Text = "Old facts";
+            this.tabPage2.UseVisualStyleBackColor = true;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(207, 222);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(88, 37);
+            this.button2.TabIndex = 0;
+            this.button2.Text = "1 день назад";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.dataGridView1.Location = new System.Drawing.Point(3, 3);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(483, 213);
+            this.dataGridView1.TabIndex = 3;
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(301, 222);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(88, 37);
+            this.button3.TabIndex = 4;
+            this.button3.Text = "2 дня назад";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(395, 222);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(88, 37);
+            this.button4.TabIndex = 5;
+            this.button4.Text = "3 дня назад";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(6, 222);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(195, 37);
+            this.button5.TabIndex = 6;
+            this.button5.Text = "Сегодня";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(497, 292);
+            this.Controls.Add(this.tabControl1);
+            this.MinimumSize = new System.Drawing.Size(513, 331);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.tabControl1.ResumeLayout(false);
+            this.tabPage1.ResumeLayout(false);
+            this.tabPage2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button back;
+        private System.Windows.Forms.Button forward;
+        private System.Windows.Forms.TabControl tabControl1;
+        private System.Windows.Forms.TabPage tabPage1;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.TabPage tabPage2;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button5;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Net;
+using System.IO;
+using Newtonsoft.Json;
+using System.Data.SqlClient;
+
+namespace CatFact
+{
+    public partial class Form1 : Form
+    {
+        DateTime today = DateTime.Today;
+        string[] facts = new string[5];
+        int current = 0;
+        private SqlConnection connection = null;
+        private SqlCommand command = null;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            connection = new SqlConnection(@"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=D:\CatFact\Database1.mdf;Integrated Security=True");
+            connection.Open();
+            if (connection.State != ConnectionState.Open)
+                MessageBox.Show("БД не подключена!");
+        }
+        private void button1_Click(object sender, EventArgs e)
+        {
+            HttpWebRequest req = (HttpWebRequest)WebRequest.Create("https://catfact.ninja/facts?limit=5&max_length=255");
+            HttpWebResponse resp = (HttpWebResponse)req.GetResponse();
+
+            Stream stream = resp.GetResponseStream();
+            StreamReader strRdr = new StreamReader(stream);
+
+            string result = strRdr.ReadToEnd();
+            dynamic json = JsonConvert.DeserializeObject(result);
+            for (int i = 0; i < 5; i++)
+                facts[i] = $"{i+1}. " + json.data[i].fact;
+
+            command = new SqlCommand("SELECT * FROM CatFact where Date=@Date", connection);
+            command.Parameters.AddWithValue("Date", today.Date);
+            if (Convert.ToString(command.ExecuteScalar()) != Convert.ToString(today.Date)) 
+            {
+                for (int i = 0; i < 5; i++)
+                {
+                    command = new SqlCommand($"INSERT INTO CatFact (Date, Fact) VALUES (@Date, @Fact)", connection);
+                    command.Parameters.AddWithValue("Date", today.Date);
+                    command.Parameters.AddWithValue("Fact", facts[i]);
+                    command.ExecuteNonQuery();
+                }
+            }
+            richTextBox1.Text = facts[0];
+        }
+        private void back_Click(object sender, EventArgs e)
+        {
+            current--;
+            if (current < 0)
+                current = 4;
+            richTextBox1.Text = facts[current];
+        }
+        private void forward_Click(object sender, EventArgs e)
+        {
+            current++;
+            if (current > 4)
+                current = 0;
+            richTextBox1.Text =  facts[current];
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            GetFactsByDate(-1);
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            GetFactsByDate(-2);
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            GetFactsByDate(-3);
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            GetFactsByDate(0);
+        }
+
+        void GetFactsByDate(int n)
+        {
+            DateTime date = DateTime.Today.AddDays(n);
+            SqlDataAdapter adapter = new SqlDataAdapter($"SELECT Date, Fact FROM CatFact where Date='{date.Month}/{date.Day}/{date.Year}'", connection);
+            DataSet set = new DataSet();
+            adapter.Fill(set);
+            dataGridView1.DataSource = set.Tables[0];
+        }
+    }
+}
